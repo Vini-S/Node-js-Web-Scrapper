1fe7deef1c638b035ae5376420813036a2f7c08e
Vinita <Vini-S>
2021-12-26 20:13:27 +0530

    Scapper edited with timedelay

--- scrapper.js
+++ scrapper.js
@@ -4,25 +4,54 @@ const {JSDOM} = jsdom;
 
 let SICCodeData = [];
 
+const getSICCode = () => {
+  return new Promise(async (resolve, reject) => {
+
+  })
+}
+
+
+const searchCompany = () => {
+    return new Promise(async (resolve, reject) => {
+      await axios.get('https://companycheck.co.uk/search?term=dual+energy').then((htmlData) => {
+        setTimeout(() => {
+          const companyDataDOM = new JSDOM(htmlData.data);
+          let companyDataList = companyDataDOM.window.document.getElementsByClassName('search-section');
+          if(companyDataList){
+            let companyCode = companyDataList[0].querySelector('.result__number').textContent;
+            let companyName = companyDataList[0].querySelector('[itemprop="legalName"]').textContent.trim().split(' ').join('-');
+            SICCodeData.push({companyCode: companyCode, companyName: companyName});
+            resolve(SICCodeData)
+          }else{
+            reject("No data found")
+          }
+
+        }, Math.random() * (10 - 2 + 1) + 2);
+      }).catch((err) => {
+        console.log("error while searching company---------", err);
+        reject(err)
+      });
+    })
+}
+
+
 (async () => {
- const htmlHit1 = await axios.get('https://companycheck.co.uk/search?term=dual+energy');
- const dom1 = new JSDOM(htmlHit1.data);
 
- const listContainers = dom1.window.document.getElementsByClassName('search-section');
- if (listContainers) {
-  //  console.log(listContainers[0].innerHTML);
-  // for(let i=0; i< listContainers.length; i++){
-    // console.log(listContainers[0].querySelector('.result__number').textContent);
-    let companyCode = listContainers[0].querySelector('.result__number').textContent;
-    let companyName = listContainers[0].querySelector('[itemprop="legalName"]').textContent.trim().split(' ').join('-');
-    console.log("companyName===========", companyName, companyCode)
-    const htmlHit2 =  await axios.get(`https://companycheck.co.uk/company/${companyCode}/${companyName}/companies-house-data`);
-    const dom2 = new JSDOM(htmlHit2.data);
-    let sicCode = dom2.window.document.getElementsByClassName('Summary__right')[0].getElementsByClassName('Summary__detail')[0].getElementsByTagName('P')[0].textContent.replace(/\s+/g, ' ').trim();
+    searchCompany().then((companyData) => {
+      console.log("in callback=======", companyData,);
+      // console.log("cmpanydata=========", companyData);
+    }).catch((err) => {
+      console.log("catch the error in main----", err);
+    });
 
-    console.log("sic code =", sicCode.trim())
-    // console.log(dom2.window.document.getElementsByClassName('Summary__right')[0].getElementsByClassName('Summary__detail')[0].innerHTML);
-  // }
+//     const htmlHit2 =  await axios.get(`https://companycheck.co.uk/company/${companyCode}/${companyName}/companies-house-data`);
+//     const dom2 = new JSDOM(htmlHit2.data);
+//     let sicCode = dom2.window.document.getElementsByClassName('Summary__right')[0].getElementsByClassName('Summary__detail')[0].getElementsByTagName('P')[0].textContent.replace(/\s+/g, ' ').trim();
+
+//     console.log("sic code =", sicCode.trim())
+//     // console.log(dom2.window.document.getElementsByClassName('Summary__right')[0].getElementsByClassName('Summary__detail')[0].innerHTML);
   
- }
+
 })();
+
+
